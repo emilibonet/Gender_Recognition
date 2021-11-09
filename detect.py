f0f63cc5cf0a51fa998bf3674b6bc62032d0aae7
Emili Bonet i Cervera <emili.bonet@est.fib.upc.edu>
2021-11-09 16:14:59 +0100

    detections has been successfully implemented

--- detect.py
+++ detect.py
@@ -1,104 +1,47 @@
 import os
-import argparse
 import numpy as np
-import cv2
-import torch
+import pandas as pd
 from tqdm import tqdm
+from time import time
 
 import utils
-import DSFD.face_ssd as mod
-import DSFD.demo as pred
-from DSFD.data import config, widerface, TestBaseTransform
-cfg = config.widerface_640
-
+import cv2
+import face_detection as fd
 
 img_dir = "data/dataset_train_samples/"
 gt_dir = "data/ground_truth_dataset_train_samples/"
 ann_dir = "data/annotated_train_samples/"
 pred_dir = "data/predicted_train_samples/"
 
-
-def arguments():
-    parser = argparse.ArgumentParser(description='DSFD:Dual Shot Face Detector')
-    parser.add_argument('--trained_model', default='DSFD/weights/WIDERFace_DSFD_RES152.pth',
-                        type=str, help='Trained state_dict file path to open')
-    parser.add_argument('--save_folder', default=pred_dir, type=str,
-                        help='Dir to save results')
-    parser.add_argument('--visual_threshold', default=0.1, type=float,
-                        help='Final confidence threshold')
-    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool,
-                        help='Use cuda to train model')
-    parser.add_argument('--img_root', default=None, help='Location of test images directory')
-    parser.add_argument('--widerface_root', default=widerface.WIDERFace_ROOT, help='Location of WIDERFACE root directory')
-    args = parser.parse_args()
-
-    if args.cuda:
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    else:
-        torch.set_default_tensor_type('torch.FloatTensor')
-    if not os.path.exists(args.save_folder):
-        os.mkdir(args.save_folder)
-    return args
-
-def load_model(cuda, trained_model):
-    net = mod.build_ssd('test', num_classes=2) # initialize SSD
-    device = 'cuda' if cuda else 'cpu'
-    net.load_state_dict(torch.load(trained_model, map_location=torch.device(device)))
-    if cuda:
-        net.cuda()
-    net.eval()
-    return net
-
-# Please excuse the mess and the shitty comments. They are not mine.
-def detect(net, img, args):
-    thresh = cfg['conf_thresh']
-    transform = TestBaseTransform((104, 117, 123))  # no sé per què mean té aquests valors ni què representen 
-    max_im_shrink = ( (2000.0*2000.0) / (img.shape[0] * img.shape[1])) ** 0.5
-    shrink = max_im_shrink if max_im_shrink < 1 else 1
-    det0 = pred.infer(net , img , transform , thresh , args.cuda , shrink)
-    det1 = pred.infer_flip(net , img , transform , thresh , args.cuda , shrink)
-    # shrink detecting and shrink only detect big face
-    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
-    det_s = pred.infer(net , img , transform , thresh , args.cuda , st)
-    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1, det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
-    det_s = det_s[index, :]
-    # enlarge one times
-    factor = 2
-    bt = min(factor, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
-    det_b = pred.infer(net , img , transform , thresh , args.cuda , bt)
-    # enlarge small iamge x times for small face
-    if max_im_shrink > factor:
-        bt *= factor
-        while bt < max_im_shrink:
-            det_b = np.row_stack((det_b, pred.infer(net , img , transform , thresh , args.cuda , bt)))
-            bt *= factor
-        det_b = np.row_stack((det_b, pred.infer(net , img , transform , thresh , args.cuda , max_im_shrink) ))
-    # enlarge only detect small face
-    if bt > 1:
-        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
-        det_b = det_b[index, :]
-    else:
-        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
-        det_b = det_b[index, :]
-    det = np.row_stack((det0, det1, det_s, det_b))
-    det = mod.bbox_vote(det)
-    return det
+def dets2df(detections):
+    return pd.DataFrame([{"gender":"undef", "bbox":[int(round(xmin)), int(round(ymin)), int(round(xmax)), int(round(ymax))]} for xmin, ymin, xmax, ymax,_ in detections])
+        
 
 if __name__ == "__main__":
-    args = arguments()
-    net = load_model(args.cuda, args.trained_model)
-    print("CUDA available:", args.cuda)
-    print("Number of parameters in the network:", sum(p.numel() for p in net.parameters() if p.requires_grad))
-    image_names = [name[:-4] for name in os.listdir(img_dir)]
+    print("Loading model...", end=" ")
+    start = time()
+    detector = fd.build_detector("DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+    print(f"done ({round(time()-start, 2)}s)")
+
+    print(f"Applying detection to images from directory {img_dir}")
+    image_names = [name[:-4] for name in os.listdir(img_dir) if name[0] != "."]
     for i,img_name in tqdm(enumerate(image_names)):
-        args.img_root = f'{img_dir}{img_name}.jpg'
-        save_path = f'{ann_dir}{img_name}.jpg'
-        img = utils.read_image(args.img_root)
-        if img is None:
-            continue
-        # gt_path = gt_dir+img_name+".csv"
-        # gt = utils.read_annotations(gt_path)
-        det = detect(net, img, args)
-        print(f"Detection results ({i}):", det)
-        print("-"*30)
-    
+        img_path, save_path = f'{img_dir}{img_name}.jpg', f'{ann_dir}{img_name}.jpg'
+        img = cv2.imread(img_path)[:, :, ::-1]
+        detections = detector.detect(img)
+        annotations = dets2df(detections)
+        print(i, annotations)
+        utils.plot_annotation(img_path, save_path, annotations)
+        
+
+"""
+import cv2
+import face_detection
+print(face_detection.available_detectors)
+detector = face_detection.build_detector(
+  "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+# BGR to RGB
+im = cv2.imread("path_to_im.jpg")[:, :, ::-1]
+
+detections = detector.detect(im)
+"""
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
 import cv2
 from ast import literal_eval  # CSV bbox as list
 
@@ -21,17 +22,24 @@ def read_image(path):
         return None
 
 
-def plot_annotation(img_path, gt_path, save_path):
-    gender2color = {"man":(255, 0, 0), "woman":(0, 0, 255)}
-    img, gt = read_image(img_path), read_annotations(gt_path)
-    if img is None or gt is None:
+def plot_annotation(img_path, save_path, annotations):
+    if type(annotations) not in [str, pd.DataFrame]:
+        print("Please provide either a path to a csv containing the annotations or a pandas dataframe.")
         return 1
-    for i in range(len(gt)):
-        start_point = (gt["bbox"][i][0], gt["bbox"][i][1])
-        end_point = (gt["bbox"][i][2], gt["bbox"][i][3])
+    if type(annotations) is str:
+        ann = read_annotations(annotations)
+    else:
+        ann = annotations
+    img = read_image(img_path)
+    gender2color = {"man":(255, 0, 0), "woman":(0, 0, 255), "undef":(0, 255, 0)}
+    if img is None or ann is None:
+        return 1
+    for i in range(len(ann)):
+        start_point = (ann["bbox"][i][0], ann["bbox"][i][1])
+        end_point = (ann["bbox"][i][2], ann["bbox"][i][3])
         # draw the rectangle
-        cv2.rectangle(img, start_point, end_point, gender2color[gt["gender"][i]], thickness=3, lineType=cv2.LINE_8) 
-        cv2.putText(img, gt["gender"][i], (start_point[0], start_point[1]-5), color=gender2color[gt["gender"][i]], fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = .35)
+        cv2.rectangle(img, start_point, end_point, gender2color[ann["gender"][i]], thickness=3, lineType=cv2.LINE_8) 
+        cv2.putText(img, ann["gender"][i], (start_point[0], start_point[1]-5), color=gender2color[ann["gender"][i]], fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = .35)
         # display the output
         cv2.imwrite(save_path, img)
     return 0
