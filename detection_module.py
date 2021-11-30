ac23a84da8a943815a4b5c7300edfdffd80c1960
Emili Bonet i Cervera <emili.bonet@est.fib.upc.edu>
2021-11-30 23:02:29 +0100

    detection prediction and ground truth pair-up

--- detection_module.py
+++ detection_module.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from time import time
@@ -14,7 +15,8 @@ pred_dir = utils.root + "data/annotations/predictions/"
 
 
 def dets2df(detections):
-    return pd.DataFrame([{"gender":"undef", "bbox":[int(round(xmin)), int(round(ymin)), int(round(xmax)), int(round(ymax))]} for xmin, ymin, xmax, ymax,_ in detections])
+    return pd.DataFrame([{"bbox":[int(round(xmin)), int(round(ymin)), int(round(xmax)), int(round(ymax))],
+                          "gender":"undef", "bbox_conf": conf} for xmin, ymin, xmax, ymax, conf in detections])
         
 
 def detect(img, detector=None):
@@ -32,8 +34,33 @@ def crop_faces(img, ann):
     return faces
 
 
+def IoU(bbox_a, bbox_b):
+    # coordinates of the intersection rectangle
+    x_min, y_min = max(bbox_a[0], bbox_b[0]), max(bbox_a[1], bbox_b[1])
+    x_max, y_max = min(bbox_a[2], bbox_b[2]), min(bbox_a[3], bbox_b[3])
+
+    # areas of bboxes a, b and intersection
+    area_a = (bbox_a[2] - bbox_a[0]) * (bbox_a[3] - bbox_a[1])
+    area_b = (bbox_b[2] - bbox_b[0]) * (bbox_b[3] - bbox_b[1])
+    area_inter = (x_max - x_min) * (y_max - y_min)
+
+    return area_inter / float(area_a + area_b - area_inter)
+
+def pred2gt_pairup(pred_df, gt_df, iou_thrs=.5):
+    pred_sorted_idx = np.argsort([conf for conf in pred_df["conf"]])
+    # Assign prediction bounding boxes to the ground truth bounding box that has the highest IoU and is over the threshold.
+    pred_adjlist = [-1]*len(pred_df)
+    for pred_idx in pred_sorted_idx:
+        gt_iousorted_idx = np.argsort([IoU(pred_df["bbox"][pred_idx], gt_bbox) for gt_bbox in gt_df["bbox"]])
+        for gt_idx in gt_iousorted_idx:
+            if not gt_idx in pred_adjlist:
+                break  # we have found an unassigned gt bbox
+        if IoU(pred_df["bbox"][pred_idx], gt_df["bbox"][gt_idx]) > iou_thrs:
+            pred_adjlist[pred_idx] = gt_idx
+    return pred_adjlist
+
 if __name__ == "__main__":
-    print("Loading model...", end=" ")
+    print("Loading detector...", end=" ")
     start = time()
     detector = fd.build_detector("DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
     print(f"done ({round(time()-start, 2)}s)")
@@ -47,7 +74,6 @@ if __name__ == "__main__":
         ann.to_csv(f'{pred_dir}{img_name}.csv', index=False)
         utils.draw_annotation(img_path, ann, save_path)
         faces = crop_faces(img, ann)
-        # Resize faces with mag shape
         std_width, std_height = 112, 112    # width, height
         faces = [cv2.resize(face, (std_width, std_height), interpolation = cv2.INTER_CUBIC) for face in faces]
         [cv2.imwrite(f"{face_dir}{img_name}_face{i}.png", face[:, :, ::-1]) for i,face in enumerate(faces)]
